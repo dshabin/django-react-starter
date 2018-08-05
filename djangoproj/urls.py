e6decde7b7e18d711637ec8a7af0cfb3b5d5e130
Shabin <d.shabin@gmail.com>
2018-08-05 13:15:36 +0400

    Change url

--- djangoproj/urls.py
+++ djangoproj/urls.py
@@ -19,6 +19,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
   path('admin/', admin.site.urls),
-  path('api/', include('apps.authapp.urls')),
+  path('api/authapp/', include('apps.authapp.urls')),
   re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
